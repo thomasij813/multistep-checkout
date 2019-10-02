d4e75c4830077811a47f1b40ae2260217c8d0918
Thomas Johnson <thomasij813@gmail.com>
2019-10-02 11:47:00 -0700

    add babel and basic react

---
+++ client/app.jsx
@@ -0,0 +1,11 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <div>hello world</div>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
